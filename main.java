e56e9a3c96aaf9911dcb82e320cf81478bcaba70
MonaPB <mona.pourbideh@gmail.com>
2024-06-11 10:58:19 +0330

    done

--- main.java
+++ main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+        String S = "XIML";
+        int sum=0;
+        for (int i =0; i<S.length(); i++)
+        {
+            char c= S.charAt(i);
+            if (c=='I')
+                sum +=1;
+            if (c=='V')
+                sum +=5;
+            if (c=='X')
+                sum +=10;
+            if (c=='L')
+                sum +=50;
+            if (c=='C')
+                sum +=100;
+            if (c=='D')
+                sum +=500;
+            if (c=='M')
+                sum +=1000;
+        }
+        System.out.println("The String is: "+S+"\n"+"The Integer Value is: "+sum);
+    }
 }
