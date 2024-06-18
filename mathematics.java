24854f949cb264d57c528d900b0782edd9b5efc4
MonaPB <mona.pourbideh@gmail.com>
2024-06-18 14:24:43 +0330

    done

--- mathematics.java
+++ mathematics.java
@@ -4,7 +4,7 @@ public class mathematics {
             System.out.println("My Number Is: "+number+"\n"+"Factorial Value Is: "+factorial(number));
         }
         public static Integer factorial(int a){
-            if (a==0){
+            if (a<=1){
                 return 1;
             } else {
                 return a * factorial(a-1);
